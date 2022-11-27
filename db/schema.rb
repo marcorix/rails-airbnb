37c2c49514255a0091a35411af0bff7100cc02d5
Marco Ricci <mr.ricci79@gmail.com>
2022-11-27 12:24:48 +0000

    rails new + flat model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_27_121707) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_27_122154) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "flats", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.integer "bedrooms"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
