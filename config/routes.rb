e194ff1b468bdda14cfa85a956f41fc95b30e7bc
Marco Ricci <mr.ricci79@gmail.com>
2022-11-29 16:11:51 +0000

    chatroom model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
+  get 'chatrooms/show'
 
   devise_for :users
   root to: "pages#home"
   get 'my-profile', to: 'pages#my_profile'
+
   resources :flats, only: ['index', 'show'] do
     resources :bookings, only: ['create']
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :chatrooms, only: :show
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_27_170334) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_29_160302) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_27_170334) do
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
+  create_table "chatrooms", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "flats", force: :cascade do |t|
     t.string "name"
     t.string "address"
@@ -35,6 +41,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_27_170334) do
     t.index ["user_id"], name: "index_flats_on_user_id"
   end
 
+  create_table "messages", force: :cascade do |t|
+    t.string "content"
+    t.bigint "chatroom_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -43,6 +59,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_27_170334) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -50,4 +67,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_27_170334) do
   add_foreign_key "bookings", "flats"
   add_foreign_key "bookings", "users"
   add_foreign_key "flats", "users"
+  add_foreign_key "messages", "chatrooms"
+  add_foreign_key "messages", "users"
 end
